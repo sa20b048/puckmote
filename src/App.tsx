772bc02684e4ade90f7621ad201dc418c0a0e566
sa20b048 <j.p.matousek@gmail.com>
2025-02-17 17:59:41 +0100

    Update App.tsx
    App use code

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,8 @@ export const App = () => {
       {deviceList.map((path) => (
         <Device key={path} path={path} />
       ))}
+
+      <IRPulseManager />  {/* Using the IRPulseManager here */}
     </div>
   );
 };
