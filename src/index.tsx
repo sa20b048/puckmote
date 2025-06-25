8216adadf19207b2f425c6a25d7f7d8e3515c2f7
sa20b048 <j.p.matousek@gmail.com>
2025-06-25 10:03:16 +0200

    Commit1

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import { App } from "./App";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-root.render(<App />);
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(<App />);
+}
+
+
