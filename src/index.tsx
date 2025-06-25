12e8fd514d5fe609ba60bd6c095d72c90eec0bdb
sa20b048 <j.p.matousek@gmail.com>
2025-06-25 10:09:34 +0200

    App

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { App } from "./App";
 import ReactDOM from "react-dom/client";
+import {App} from "./App";
 
 const rootElement = document.getElementById("root");
 
@@ -8,5 +8,3 @@ if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(<App />);
 }
-
-
