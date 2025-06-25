749ed8f0891ff1fd8efd05f75c946cf9e32295c9
sa20b048 <j.p.matousek@gmail.com>
2025-06-25 09:49:17 +0200

    React Update

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,6 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import { App } from "./App";
 
-// Sicherstellen, dass ein Element mit ID 'root' existiert
-const container = document.getElementById("root");
-
-if (!container) {
-  throw new Error("Root container missing in index.html");
-}
-
-const root = createRoot(container); // 💡 React 18 API
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+root.render(<App />);
