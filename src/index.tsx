206dac14d23c6b0aa6c810a348d13bd65aaf14a3
sa20b048 <j.p.matousek@gmail.com>
2025-06-25 09:23:51 +0200

    index react dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+
 import { App } from "./App";
 
-const container = document.getElementById("root");
-if (!container) throw new Error("Root container missing");
 
-const root = ReactDOM.createRoot(container);
-root.render(<App />);
+import { createRoot } from 'react-dom/client'
+createRoot(document.getElementById('root')).render(<h1>Your App</h1>)
