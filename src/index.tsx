b4b3e26b65a9999b1e5e9fcc912f76e75e771c5d
sa20b048 <j.p.matousek@gmail.com>
2025-06-25 09:26:18 +0200

    React Root

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
-
+import { createRoot } from 'react-dom/client';
 import { App } from "./App";
 
-
-import { createRoot } from 'react-dom/client'
-createRoot(document.getElementById('root')).render(<h1>Your App</h1>)
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
